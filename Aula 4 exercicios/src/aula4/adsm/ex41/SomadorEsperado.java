7df17e0f7c805bab629448cf1bbd8c19be8ebc31
guizpereira <62037075+guizpereira@users.noreply.github.com>
2020-04-04 15:28:25 -0300

    Atualizado dia 04/04

---
+++ Aula 4 exercicios/src/aula4/adsm/ex41/SomadorEsperado.java
@@ -0,0 +1,5 @@
+package aula4.adsm.ex41;
+
+public interface SomadorEsperado{
+	int somaVetor(int[] vetor);
+}
---
+++ Aula 4 exercicios/src/aula4/adsm/ex61/TesteMensagemDoDia.java
@@ -0,0 +1,12 @@
+package aula4.adsm.ex61;
+
+public class TesteMensagemDoDia {
+	public static void main(String[] args) {
+		
+		Sorteador mensagem = new Sorteador();
+		mensagem.mensagemDoDia();
+		
+			
+	}
+
+}
